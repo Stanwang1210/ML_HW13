e9080a47c2f044c00426f6369250ded947045182
Stanwang1210 <ch995308@gmail.com>
2020-06-22 13:16:29 +0800

    'upload'

--- p2.py
+++ p2.py
@@ -161,7 +161,7 @@ def MAML(model, optimizer, x, n_way, k_shot, q_query, loss_fn, inner_train_step 
     val_set = meta_batch[n_way*k_shot:]   # val_set 是我們拿來 update outer loop 參數的 data
     
     fast_weights = OrderedDict(model.named_parameters()) # 在 inner loop update 參數時，我們不能動到實際參數，因此用 fast_weights 來儲存新的參數 θ'
-    st = [0. for i in range(model.named_parameters().shape())]
+    st = [0. for i in model.named_parameters()]
     eps = 1e-8
     for inner_step in range(inner_train_steps): # 這個 for loop 是 Algorithm2 的 line 7~8
                                                 # 實際上我們 inner loop 只有 update 一次 gradients，不過某些 task 可能會需要多次 update inner loop 的 θ'，
